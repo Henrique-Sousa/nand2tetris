62731176aff5a1ec2228d5ec4d51cf72d2c8231a
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-08-26 18:19:41 -0300

    implement functionality to blacken the screen

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,52 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Pseudocode:
+// blackens screen
+//
+// addr = SCREEN
+// n = 8191
+// i = 0
+// LOOP
+//    if i > n goto END
+//    RAM[addr] = -1
+//    addr = addr + 1
+//    i = i + 1
+//    goto LOOP
+// END
+
+  @SCREEN
+  D=A
+  @addr
+  M=D
+  @8191
+  D=A
+  @n
+  M=D
+  @i
+  M=0
+(LOOP)
+  @i
+  D=M
+  @n
+  D=D-M
+  @END
+  D;JGT
+  @addr
+  A=M
+  M=-1
+  @addr
+  M=M+1
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
+  
+// LOOP
+//   if KBD != 0
+//     
+//   goto LOOP
