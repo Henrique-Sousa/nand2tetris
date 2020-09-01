560b56ee7f5f0dc633ba1dd259b1d2df20b7c195
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-01 09:58:26 -0300

    complete assembler (version without labels)

--- 06/assembler/assembler.py
+++ 06/assembler/assembler.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 import sys
+import os
 from parser import Parser
 from code import Code
 
 filename = sys.argv[1]
 parser = Parser(filename)
 
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+basename = os.path.basename(filename)
+outfilename = os.path.join(current_directory, os.path.splitext(basename)[0] + '.hack')
+outfile = open(outfilename, 'w')
+
 while parser.hasMoreCommands():
     if parser.getCommandType() == 'A':
-        print(Code.toBinary(parser.number(), 16))
+        out = Code.toBinary(parser.number(), 16)
     else:
         d = parser.dest() 
         c = parser.comp() 
@@ -17,8 +24,9 @@ while parser.hasMoreCommands():
         cc = Code.comp(c)
         jj = Code.jump(j)
         out = "111" + cc + dd + jj
-        print(out)
+    outfile.write(out + '\n')
     parser.advance()
 
+outfile.close()
 del(parser)
 
