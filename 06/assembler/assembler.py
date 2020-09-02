1af17f1e4a38dacf0c97f2ebb9f76521e321f7d2
Henrique-Sousa <29417378+Henrique-Sousa@users.noreply.github.com>
2020-09-02 13:12:03 -0300

    add symbol handling capability to the translating loop

--- 06/assembler/assembler.py
+++ 06/assembler/assembler.py
@@ -3,19 +3,31 @@ import sys
 import os
 from parser import Parser
 from code import Code
+from symboltable import SymbolTable;
 
 filename = sys.argv[1]
+
+symbol_table = SymbolTable()
+
 parser = Parser(filename)
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-
 basename = os.path.basename(filename)
 outfilename = os.path.join(current_directory, os.path.splitext(basename)[0] + '.hack')
 outfile = open(outfilename, 'w')
 
+next_available_ram_address = '16'
+
 while parser.hasMoreCommands():
     if parser.getCommandType() == 'A':
-        out = Code.toBinary(parser.number(), 16)
+        value = parser.address()
+        if parser.isSymbol():
+            # add new symbol to the table if it's not already there
+            if not symbol_table.getAddress(value):
+                symbol_table.updateTable(value, next_available_ram_address)
+                next_available_ram_address = str(int(next_available_ram_address) + 1)
+            value = symbol_table.getAddress(value)
+        out = Code.toBinary(value, 16)
     else:
         d = parser.dest() 
         c = parser.comp() 
@@ -29,4 +41,4 @@ while parser.hasMoreCommands():
 
 outfile.close()
 del(parser)
-
+del(symbol_table)
